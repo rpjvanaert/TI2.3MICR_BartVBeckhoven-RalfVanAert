14734d8a7be185e8d1c4703dbd65364e4e829336
Logophilist <ralf@van-aert.nl>
2021-02-11 14:42:43 +0000

    B7 comments added

--- Week 1/B.7a/B.7a/main.c
+++ Week 1/B.7a/B.7a/main.c
@@ -7,9 +7,13 @@
 
 #include <xc.h>
 
+/* 3 states; start, running and done */
 typedef enum { START, RUNNING, DONE } ENUM_STATES;
+	
+/* 2 events; long press and a short press */
 typedef enum { LONG_PRESS, SHORT_PRESS } ENUM_EVENTS;
 	
+/* The navigation array is returns the state the machine should go to depending on the input (ENUM_STATES, ENUM_EVENTS) */
 ENUM_STATES navigation[3][2] = {
 	{
 		{
@@ -24,9 +28,12 @@ ENUM_STATES navigation[3][2] = {
 	}
 };
 
+// Start with state START
 ENUM_STATES state = START;
 
+/* Sets state depending on event with navigation array */
 void handleEvent(ENUM_EVENTS event){
+	// Potentially do something here...
 	state = navigation[state][event];
 }
 
@@ -34,6 +41,7 @@ int main(void)
 {
     while(1)
     {
+		// Example
         handleEvent(SHORT_PRESS);
 		handleEvent(LONG_PRESS);
 		handleEvent(SHORT_PRESS);
