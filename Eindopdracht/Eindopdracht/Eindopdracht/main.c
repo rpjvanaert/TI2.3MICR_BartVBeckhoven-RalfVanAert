50a090a7784b0bdc2a2fbebf78b41dc5e1ece515
Logophilist <ralf@van-aert.nl>
2021-04-06 14:12:59 +0100

    Eindopdracht done

--- Eindopdracht/Eindopdracht/Eindopdracht/main.c
+++ Eindopdracht/Eindopdracht/Eindopdracht/main.c
@@ -16,15 +16,14 @@
 #define POT_METER_1 0b0000
 #define POT_METER_2 0b0001
 
-#define SERVO_1 1
-#define SERVO_2 2
+int pot_1_val;
+int pot_2_val;
 
-#define SERVO_MAX_STATE 2000
-#define SERVO_MIN_STATE 1000
-int servo1_state;
-int servo1_goal;
-int servo2_state;
-int servo2_goal;
+#define SERVO_FREQ 50
+
+#define SERVO_MAX_STATE 2500
+#define SERVO_MIN_STATE 500
+#define ADC_TO_SERVO_FACTOR 8
 
 // wait busy waiting for ms in milliseconds
 void wait( int ms )
@@ -35,7 +34,7 @@ void wait( int ms )
 	}
 }
 
-// Initialize ADC for given pin (0b0000 or 0b0001
+// Initialize ADC for given pin (0b0000 or 0b0001)
 void adc_set_port( int bit )
 {
 	ADMUX = (0b01100000 | bit); // AREF = VCC, result left adjusted, channel1 at PF1.
@@ -51,63 +50,35 @@ int adc_read_port ( int bit )
 }
 
 
-
-
-// Initialize Servo for
+// Initialize PWM for servos
 void servos_init( void )
-{
-	servo1_state = SERVO_MIN_STATE;
-	servo2_state = SERVO_MIN_STATE;
-	
-	DDRD = 0xFF;
+{	
+	// Set ports to output
 	DDRB = 0xFF;
 	DDRE = 0xFF;
-	ICR1 = 20000;
-	ICR3 = 20000;
 	
-	OCR1A = servo1_state;
-	OCR3A = servo1_state;
 	
-	TCCR1A = 0b10000010;
-	TCCR1B = 0b00011010;
+	ICR1 = F_CPU / SERVO_FREQ;
+	ICR3 = F_CPU / SERVO_FREQ;
 	
-	TCCR3A = 0b10000010;
-	TCCR3B = 0b00011010;
+	// Set PWM's to min value of servos
+	OCR1A = SERVO_MIN_STATE;
+	OCR3A = SERVO_MIN_STATE;
 	
-}
-
-/*void servo_send(int servo_nr, int pwm)
-{
-	if (servo_nr == SERVO_1)
-	{
-		OCR1A = ICR1 - pwm;
-	}
-	if (servo_nr == SERVO_2)
-	{
-		OCR1B = ICR1 - pwm;
-	}
+	// Init Timer1
+	TCCR1A = 1<<COM1A1 | 1<<WGM11;
+	TCCR1B = 1<<WGM13 | 1<<WGM12 | 1<<CS11;
+	
+	// Init Timer3
+	TCCR3A = 1<<COM3A1 | 1<<WGM31;
+	TCCR3B = 1<<WGM33 | 1<<WGM32 | 1<<CS31;
 	
 }
-*/
 
-void servos_step(){
-	if (servo1_state < servo1_goal){
-		servo1_state += 1;
-	} else if (servo1_state > servo1_goal){
-		servo1_state -= 1;
-	}
-	
-	if (servo2_state < servo2_goal){
-		servo2_state += 1;
-	} else if (servo2_state > servo2_goal){
-		servo2_state -= 1;
-	}
-	
-	OCR1A = servo1_state;
-	OCR3A = servo2_state;
+// Calculates the adc measurement to servo value
+int servoSpeed(int adc_pot_val){
+	return SERVO_MIN_STATE + adc_pot_val * ADC_TO_SERVO_FACTOR;
 }
-
-
 
 
 int main(void)
@@ -116,60 +87,27 @@ int main(void)
 	ADCSRA = 0b10000110; // ADC enabled, no interrupt, no free running, division by 64.
 	DDRF = 0x00;
 	DDRA = 0xFF;
-	DDRB = 0xFF;
-	
-	wait(1000);
-	
-	servo1_goal = 2000;
-	servo2_goal = 2000;
-	
-	servo1_state = servo1_goal;
-	servo2_state = servo2_goal;
-	
-	OCR1A = servo1_goal;
-	OCR3A = servo2_goal;
-	
+	DDRC = 0xFF;	
 	
     while(1)
-    {		
-		PORTA = adc_read_port(POT_METER_1);
-		PORTB = adc_read_port(POT_METER_2);
+    {
+		//Measure potentiometers
+		pot_1_val = adc_read_port(POT_METER_1);
+		pot_2_val = adc_read_port(POT_METER_2);
 		
-		/*
-		//servo_send(SERVO_1, 1);
+		//Show potentiometers value on board with LED's
+		PORTA = pot_1_val;
+		PORTC = pot_2_val;
 		
-		OCR1A = 2125;
+		//Adjust servo motor 1 according to potentiometer 1
+		OCR1A = servoSpeed(pot_1_val);
+	
+		//Adjust servo motor 2 according to potentiometer 2
+		OCR3A = servoSpeed(pot_2_val);
 		
-		wait(2500);
+		//Wait a bit to rest for servo
+		wait(100);
 		
-		//servo_send(SERVO_1, 2);
-		
-		OCR3A = 1800;
-		OCR1A = 1800;
-		
-		wait(2500);
-		
-		OCR3A = 1700;
-		OCR1A = 1700;
-		
-		wait(2500);
-		*/
-		
-		servos_step();
-		
-		wait(10000);
-		
-		if (servo1_state == SERVO_MAX_STATE){
-			servo1_goal = SERVO_MIN_STATE;
-		} else if (servo1_state == SERVO_MIN_STATE){
-			servo1_goal = SERVO_MAX_STATE;
-		}
-		
-		if (servo2_state == SERVO_MAX_STATE){
-			servo2_goal = SERVO_MIN_STATE;
-			} else if (servo2_state == SERVO_MIN_STATE){
-			servo2_goal = SERVO_MAX_STATE;
-		}
 		
     }
 }
